18303f95c46dc316648a174798d423b91d1c26bd
Vinoth <Vinoth@Vinoth.home>
2024-01-19 11:40:42 +0100

    Selenium Basics

---
+++ src/main/java/org/sel/SeleniumDayOne.java
@@ -0,0 +1,19 @@
+package org.sel;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class SeleniumDayOne {
+
+	public static void main(String[] args) {
+		
+		WebDriver driver = new EdgeDriver();
+		driver.get("https://rahulshettyacademy.com/");
+		System.out.println(driver.getTitle());
+		System.out.println(driver.getCurrentUrl());
+		//driver.close();
+	}
+
+}
